263d1d71665096c97eba936967b5d07c7781b8a8
DragonMaster <DragonMaster@joels-mac-mini.fios-router.home>
2016-11-11 21:16:20 -0800

    Nov 11, 2016 - Modified GameMenuView

--- HideAndSeek/src/byui/cit260/HideAndSeek/view/GameMenuView.java
+++ HideAndSeek/src/byui/cit260/HideAndSeek/view/GameMenuView.java
@@ -13,29 +13,11 @@ import java.util.Scanner;
  *
  * @author DragonMaster
  */
-class GameMenuView {
+public class GameMenuView extends View{
 
-    void displayMenu() {
-        
-        boolean done = false; // set flag to not done
-        do {
-            // prompt for and get pplauers name
-            String menuOption = this.getMenuOption();
-            if (menuOption.toUpperCase().equals("Q")) // user wants to quit
-                return; //exit the game
-            
-            // do the requested action and display the next view
-            done = this.doAction(menuOption);
-        } while (!done);
-    }
 
-    private String getMenuOption() {
-        Scanner keyboard = new Scanner(System.in); // get infile for keyboard
-        String value =""; //value to be returned
-        boolean valid = false; //initialize to not valid
-        
-        while (!valid){//loop while an invalid value is entered
-            System.out.println("\n"
+    public GameMenuView() {
+            super("\n"
                 + "\n-----------------------------------------------"
                 + "\n| Game Menu                                    "
                 + "\n-----------------------------------------------"
@@ -51,24 +33,14 @@ class GameMenuView {
                 + "\nS - Save Game"
                 + "\nQ - Quit"
                 + "\n-----------------------------------------------");
-        
-            value = keyboard.nextLine();//get next line typed on keyboard
-            value = value.trim();//trim off leading and trailing blanks
-            
-            if (value.length()< 1){// value is blank
-               System.out.println("\nInvalid value: Value can not be blank") ;
-               continue;
-            }
-            break; //end the loop
-        }
-        return value; // return the value entered
     }
 
-    private boolean doAction(String choice) {
+    @Override
+    public boolean doAction(String value) {
     
-        choice = choice.toUpperCase(); // convert choice to upper case
+        value = value.toUpperCase(); // convert value to upper case
 
-        switch (choice) {
+        switch (value) {
             case "U": // Move up
                 this.moveUp();
                 break;
