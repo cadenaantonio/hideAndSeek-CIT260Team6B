358e893f21cb73274a71e3df89908c4de4822ca1
DragonMaster <DragonMaster@joels-mac-mini.fios-router.home>
2016-12-05 12:43:20 -0800

    Dec 5, 2016 - Added exceptions to InventoryControl and QuestionControl.

--- HideAndSeek/src/byui/cit260/HideAndSeek/control/InventoryControl.java
+++ HideAndSeek/src/byui/cit260/HideAndSeek/control/InventoryControl.java
@@ -5,34 +5,44 @@
  */
 package byui.cit260.HideAndSeek.control;
 
+import byui.cit260.HideAndSeek.exceptions.InventoryControlException;
+
 /**
  *
  * @author DragonMaster
  */
 public class InventoryControl {
 
-    public int addToInventory(int item, int amount) {
+    public int addToInventory(int item, int amount) throws InventoryControlException {
 
         if (item < 0 || item > 15) {
-            return -1;
+            
+            throw new InventoryControlException("Items are are out of bounds.");
+//            return -1;
         }
 
         if (amount < 0 || amount > 15) {
-            return -1;
+            
+            throw new InventoryControlException("Amount is out of bounds.");
+//            return -1;
         }
 
         int addToInventory = item + amount;
         return addToInventory;
     }
 
-    public int useFromInventory(int item, int amount) {
+    public int useFromInventory(int item, int amount) throws InventoryControlException {
 
         if (item < 0 || item > 15) {
-            return -1;
+            
+            throw new InventoryControlException("Items are are out of bounds.");
+//            return -1;
         }
 
         if (amount < 0 || amount > 15 || amount > item) {
-            return -1;
+            
+            throw new InventoryControlException("Amount is out of bounds.");
+//            return -1;
         }
 
         int useFromInventory = item - amount;
